e93840f5c2049e24b368d421307f4c263493ce51
Adam Mus <adammuspraca@gmail.com>
2026-01-04 16:36:32 +0100

    Wygenerowanie kontrolerów i widoków MVC dla Produktów, Kategorii i Zamówień

---
+++ Controllers/ProduktyController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CukierniaAdamMus.Models;
+
+namespace CukierniaAdamMus.Controllers
+{
+    public class ProduktyController : Controller
+    {
+        private readonly CukierniaContext _context;
+
+        public ProduktyController(CukierniaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Produkty
+        public async Task<IActionResult> Index()
+        {
+            var cukierniaContext = _context.Produkty.Include(p => p.Kategoria);
+            return View(await cukierniaContext.ToListAsync());
+        }
+
+        // GET: Produkty/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var produkt = await _context.Produkty
+                .Include(p => p.Kategoria)
+                .FirstOrDefaultAsync(m => m.ProduktId == id);
+            if (produkt == null)
+            {
+                return NotFound();
+            }
+
+            return View(produkt);
+        }
+
+        // GET: Produkty/Create
+        public IActionResult Create()
+        {
+            ViewData["KategoriaId"] = new SelectList(_context.Kategorie, "KategoriaId", "Nazwa");
+            return View();
+        }
+
+        // POST: Produkty/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProduktId,Nazwa,Opis,Cena,KategoriaId")] Produkt produkt)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(produkt);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["KategoriaId"] = new SelectList(_context.Kategorie, "KategoriaId", "Nazwa", produkt.KategoriaId);
+            return View(produkt);
+        }
+
+        // GET: Produkty/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var produkt = await _context.Produkty.FindAsync(id);
+            if (produkt == null)
+            {
+                return NotFound();
+            }
+            ViewData["KategoriaId"] = new SelectList(_context.Kategorie, "KategoriaId", "Nazwa", produkt.KategoriaId);
+            return View(produkt);
+        }
+
+        // POST: Produkty/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProduktId,Nazwa,Opis,Cena,KategoriaId")] Produkt produkt)
+        {
+            if (id != produkt.ProduktId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(produkt);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProduktExists(produkt.ProduktId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["KategoriaId"] = new SelectList(_context.Kategorie, "KategoriaId", "Nazwa", produkt.KategoriaId);
+            return View(produkt);
+        }
+
+        // GET: Produkty/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var produkt = await _context.Produkty
+                .Include(p => p.Kategoria)
+                .FirstOrDefaultAsync(m => m.ProduktId == id);
+            if (produkt == null)
+            {
+                return NotFound();
+            }
+
+            return View(produkt);
+        }
+
+        // POST: Produkty/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var produkt = await _context.Produkty.FindAsync(id);
+            if (produkt != null)
+            {
+                _context.Produkty.Remove(produkt);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProduktExists(int id)
+        {
+            return _context.Produkty.Any(e => e.ProduktId == id);
+        }
+    }
+}
---
+++ Controllers/ZamowieniaController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CukierniaAdamMus.Models;
+
+namespace CukierniaAdamMus.Controllers
+{
+    public class ZamowieniaController : Controller
+    {
+        private readonly CukierniaContext _context;
+
+        public ZamowieniaController(CukierniaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Zamowienia
+        public async Task<IActionResult> Index()
+        {
+            var cukierniaContext = _context.Zamowienia.Include(z => z.Klient);
+            return View(await cukierniaContext.ToListAsync());
+        }
+
+        // GET: Zamowienia/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var zamowienie = await _context.Zamowienia
+                .Include(z => z.Klient)
+                .FirstOrDefaultAsync(m => m.ZamowienieId == id);
+            if (zamowienie == null)
+            {
+                return NotFound();
+            }
+
+            return View(zamowienie);
+        }
+
+        // GET: Zamowienia/Create
+        public IActionResult Create()
+        {
+            ViewData["KlientId"] = new SelectList(_context.Klienci, "KlientId", "Email");
+            return View();
+        }
+
+        // POST: Zamowienia/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ZamowienieId,DataZamowienia,WartoscZamowienia,KlientId")] Zamowienie zamowienie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(zamowienie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["KlientId"] = new SelectList(_context.Klienci, "KlientId", "Email", zamowienie.KlientId);
+            return View(zamowienie);
+        }
+
+        // GET: Zamowienia/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var zamowienie = await _context.Zamowienia.FindAsync(id);
+            if (zamowienie == null)
+            {
+                return NotFound();
+            }
+            ViewData["KlientId"] = new SelectList(_context.Klienci, "KlientId", "Email", zamowienie.KlientId);
+            return View(zamowienie);
+        }
+
+        // POST: Zamowienia/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ZamowienieId,DataZamowienia,WartoscZamowienia,KlientId")] Zamowienie zamowienie)
+        {
+            if (id != zamowienie.ZamowienieId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(zamowienie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ZamowienieExists(zamowienie.ZamowienieId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["KlientId"] = new SelectList(_context.Klienci, "KlientId", "Email", zamowienie.KlientId);
+            return View(zamowienie);
+        }
+
+        // GET: Zamowienia/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var zamowienie = await _context.Zamowienia
+                .Include(z => z.Klient)
+                .FirstOrDefaultAsync(m => m.ZamowienieId == id);
+            if (zamowienie == null)
+            {
+                return NotFound();
+            }
+
+            return View(zamowienie);
+        }
+
+        // POST: Zamowienia/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var zamowienie = await _context.Zamowienia.FindAsync(id);
+            if (zamowienie != null)
+            {
+                _context.Zamowienia.Remove(zamowienie);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ZamowienieExists(int id)
+        {
+            return _context.Zamowienia.Any(e => e.ZamowienieId == id);
+        }
+    }
+}
