453385c4579a02df33362e8263de75fbf8d6c2d2
Adam Mus <adammuspraca@gmail.com>
2026-01-11 01:55:01 +0100

    Dodano klienciController.cs

---
+++ Controllers/KlienciController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CukierniaAdamMus.Models;
+
+namespace CukierniaAdamMus.Controllers
+{
+    public class KlienciController : Controller
+    {
+        private readonly CukierniaContext _context;
+
+        public KlienciController(CukierniaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Klienci
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Klienci.ToListAsync());
+        }
+
+        // GET: Klienci/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var klient = await _context.Klienci
+                .FirstOrDefaultAsync(m => m.KlientId == id);
+            if (klient == null)
+            {
+                return NotFound();
+            }
+
+            return View(klient);
+        }
+
+        // GET: Klienci/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Klienci/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("KlientId,Imie,Nazwisko,Telefon,Email")] Klient klient)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(klient);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(klient);
+        }
+
+        // GET: Klienci/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var klient = await _context.Klienci.FindAsync(id);
+            if (klient == null)
+            {
+                return NotFound();
+            }
+            return View(klient);
+        }
+
+        // POST: Klienci/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("KlientId,Imie,Nazwisko,Telefon,Email")] Klient klient)
+        {
+            if (id != klient.KlientId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(klient);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!KlientExists(klient.KlientId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(klient);
+        }
+
+        // GET: Klienci/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var klient = await _context.Klienci
+                .FirstOrDefaultAsync(m => m.KlientId == id);
+            if (klient == null)
+            {
+                return NotFound();
+            }
+
+            return View(klient);
+        }
+
+        // POST: Klienci/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var klient = await _context.Klienci.FindAsync(id);
+            if (klient != null)
+            {
+                _context.Klienci.Remove(klient);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool KlientExists(int id)
+        {
+            return _context.Klienci.Any(e => e.KlientId == id);
+        }
+    }
+}
--- Models/Zamowienie.cs
+++ Models/Zamowienie.cs
@@ -7,16 +7,22 @@ namespace CukierniaAdamMus.Models
     {
         [Key]
         public int ZamowienieId { get; set; }
+
         [Required(ErrorMessage = "Data zamówienia jest wymagana.")]
         [Display(Name = "Data Zamówienia")]
         public DateTime DataZamowienia { get; set; }
 
+        [Required]
         [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal WartoscZamowienia { get; set; }
+
         [Display(Name = "Klient")]
         public int KlientId { get; set; }
+
         [ForeignKey("KlientId")]
         public virtual Klient? Klient { get; set; }
+
         public virtual ICollection<PozycjaZamowienia>? PozycjaZamowienias { get; set; }
     }
 }
